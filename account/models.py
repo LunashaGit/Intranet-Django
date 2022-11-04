56e9eb45eb69e35fb6b44ecfaa0c099096c41eeb
LunashaGit <lunamuylkens@gmail.com>
2022-11-04 09:43:15 +0100

    Profiles Categories Products & Images(->Products) to admin Panel Django

--- account/models.py
+++ account/models.py
@@ -1,3 +1,14 @@
 from django.db import models
-
+from base.models import BaseModel
+from django.contrib.auth.models import User
 # Create your models here.
+
+class Profile(BaseModel):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    profile_image = models.ImageField(upload_to="profile", null=True, blank=True)
+    bio = models.TextField(null=True, blank=True)
+    phone = models.CharField(max_length=255, null=True, blank=True)
+    address = models.CharField(max_length=255, null=True, blank=True)
+
+    def __str__(self):
+        return self.user.username
--- appli/settings.py
+++ appli/settings.py
@@ -42,6 +42,7 @@ INSTALLED_APPS = [
     'django_browser_reload',
     'intranet',
     'projects',
+    'account',
 ]
 
 MIDDLEWARE = [
